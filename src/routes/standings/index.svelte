2269c796139fb407a1655507a8e4bcb04ddf8b98
Tots <tots@Kates-MacBook-Pro-2.local>
2022-07-03 22:05:02 -0500

    finalize roulette standings spreadsheet

--- src/routes/standings/index.svelte
+++ src/routes/standings/index.svelte
@@ -37,7 +37,7 @@
 
 <div class="standings">
 	<p>
-		<a href = "https://docs.google.com/spreadsheets/d/1Pa8a-XfLJ0OwoVeE1l5DMFiK8Pv1z8HzUmiRn-Pi8rQ/edit?usp=sharing">test</a>
+		<a href = "https://docs.google.com/spreadsheets/d/1Pa8a-XfLJ0OwoVeE1l5DMFiK8Pv1z8HzUmiRn-Pi8rQ/edit?usp=sharing">Link to Roulette Standings</a>
 	</p>
 </div>
 
