cdb3708efece0e013875c6022939bc53a88acaec
Tots <tots@Kates-MacBook-Pro-2.local>
2022-07-04 14:05:12 -0500

    add roulette standings iframe

--- src/routes/standings/index.svelte
+++ src/routes/standings/index.svelte
@@ -39,6 +39,9 @@
 	<p>
 		<a href = "https://docs.google.com/spreadsheets/d/1Pa8a-XfLJ0OwoVeE1l5DMFiK8Pv1z8HzUmiRn-Pi8rQ/edit?usp=sharing">Link to Roulette Standings</a>
 	</p>
+	<p>
+		<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS106eSbm5q9GRkNQgZGE_KKfEwzFauiETdzEvOpOL1BxcvjqLhLYGuAAXYqrCLHw5zCastgEIRR7Is/pubhtml?gid=1501533110&amp;range=A1:AH13&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+	</p>
 </div>
 
 
